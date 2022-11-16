b2db56d663c6a65c00917739c2ef279a581a53ae
Mateus <mateus.bastos@hotmail.com>
2022-11-16 13:03:48 -0300

    Update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function createGame(player1, hour, player2, estadio) {
+function createGame(player1, hour, player2, estadio, grupo) {
   return `
-  <li>
+  <li class="${grupo}">
     <div class="flagLeft">
     <img src="./assets/icon-${player1}.svg" alt="Bandeira ${player1}">
     <figcaption class="teamName">${player1}</figcaption>
@@ -47,120 +47,107 @@ function estadio(numero) {
 }
 
 document.querySelector("#app").innerHTML = `
-<header>
-    <img src="./assets/logo.svg" alt="Logo da NWL">
-    <br>
-    <span class="button">Fase de grupos</span>
-    <span class="button">Fase de grupos</span>
-    <span class="button">Fase de grupos</span>
-    <span class="button">Fase de grupos</span>
-</header>
 <main id="cards">
   ${createCard(
     "20/11",
     "domingo",
-    createGame("catar", "13:00", "equador", estadio(5) /*group a*/)
+    createGame("catar", "13:00", "equador", estadio(5), "cgrupoa")
   )}
   ${createCard(
     "21/11",
     "segunda",
-    createGame("inglaterra", "10:00", "irã", estadio(1) /*group b*/) +
-      createGame("senegal", "13:00", "holanda", estadio(2) /*group a*/) +
-      createGame("estados unidos", "16:00", "gales", estadio(5) /*group b*/)
+    createGame("inglaterra", "10:00", "irã", estadio(1), "cgrupob") +
+      createGame("senegal", "13:00", "holanda", estadio(2), "cgrupoa") +
+      createGame("estados unidos", "16:00", "gales", estadio(5), "cgrupob")
   )}
   ${createCard(
     "22/11",
     "terça",
-    createGame("argentina", "07:00", "arábia saudita", estadio(0) /*group c*/) +
-      createGame("dinamarca", "10:00", "tunísia", estadio(7) /*group d*/) +
-      createGame("méxico", "13:00", "polônia", estadio(6) /*group c*/) +
-      createGame("frança", "16:00", "austrália", estadio(3) /*group d*/)
+    createGame("argentina", "07:00", "arábia saudita", estadio(0), "cgrupoc") +
+      createGame("dinamarca", "10:00", "tunísia", estadio(7), "cgrupod") +
+      createGame("méxico", "13:00", "polônia", estadio(6), "cgrupoc") +
+      createGame("frança", "16:00", "austrália", estadio(3), "cgrupod")
   )}
   ${createCard(
     "23/11",
     "quarta",
-    createGame("marrocos", "07:00", "croácia", estadio(4) /*group f*/) +
-      createGame("alemanha", "10:00", "japão", estadio(1) /*group e*/) +
-      createGame("espanha", "13:00", "costa rica", estadio(2) /*group e*/) +
-      createGame("bélgica", "16:00", "canadá", estadio(5) /*group f*/)
+    createGame("marrocos", "07:00", "croácia", estadio(4), "cgrupof") +
+      createGame("alemanha", "10:00", "japão", estadio(1), "cgrupoe") +
+      createGame("espanha", "13:00", "costa rica", estadio(2), "cgrupoe") +
+      createGame("bélgica", "16:00", "canadá", estadio(5), "cgrupof")
   )}
   ${createCard(
     "24/11",
     "quinta",
-    createGame("suíça", "07:00", "camarões", estadio(3) /*group g*/) +
-      createGame("uruguai", "10:00", "coreia do sul", estadio(7) /*group h*/) +
-      createGame("portugal", "13:00", "gana", estadio(6) /*group h*/) +
-      createGame("brasil", "16:00", "sérvia", estadio(0) /*group g*/)
+    createGame("suíça", "07:00", "camarões", estadio(3), "cgrupog") +
+      createGame("uruguai", "10:00", "coreia do sul", estadio(7), "cgrupoh") +
+      createGame("portugal", "13:00", "gana", estadio(6), "cgrupoh") +
+      createGame("brasil", "16:00", "sérvia", estadio(0), "cgrupog")
   )}
   ${createCard(
     "25/11",
     "sexta",
-    createGame("gales", "07:00", "irã", estadio(5) /*group b*/) +
-      createGame("catar", "10:00", "senegal", estadio(2) /*group a*/) +
-      createGame("holanda", "13:00", "equador", estadio(1) /*group a*/) +
-      createGame(
-        "inglaterra",
-        "16:00",
-        "estados unidos",
-        estadio(4) /*group b*/
-      )
+    createGame("gales", "07:00", "irã", estadio(5), "cgrupob") +
+      createGame("catar", "10:00", "senegal", estadio(2), "cgrupoa") +
+      createGame("holanda", "13:00", "equador", estadio(1), "cgrupoa") +
+      createGame("inglaterra", "16:00", "estados unidos", estadio(4), "cgrupob")
   )}
   ${createCard(
     "26/11",
     "sábado",
-    createGame("tunísia", "07:00", "austrália", estadio(3) /*group d*/) +
-      createGame("polônia", "10:00", "arábia saudita", estadio(7) /*group c*/) +
-      createGame("frança", "13:00", "dinamarca", estadio(6) /*group d*/) +
-      createGame("argentina", "16:00", "méxico", estadio(0) /*group c*/)
+    createGame("tunísia", "07:00", "austrália", estadio(3), "cgrupod") +
+      createGame("polônia", "10:00", "arábia saudita", estadio(7), "cgrupoc") +
+      createGame("frança", "13:00", "dinamarca", estadio(6), "cgrupod") +
+      createGame("argentina", "16:00", "méxico", estadio(0), "cgrupoc")
   )}
 
   ${createCard(
     "27/11",
     "domingo",
-    createGame("japão", "07:00", "costa rica", estadio(5) /*group e*/) +
-      createGame("bélgica", "10:00", "marrocos", estadio(2) /*group f*/) +
-      createGame("croácia", "13:00", "canadá", estadio(1) /*group f*/) +
-      createGame("espanha", "16:00", "alemanha", estadio(4) /*group e*/)
+    createGame("japão", "07:00", "costa rica", estadio(5), "cgrupoe") +
+      createGame("bélgica", "10:00", "marrocos", estadio(2), "cgrupof") +
+      createGame("croácia", "13:00", "canadá", estadio(1), "cgrupof") +
+      createGame("espanha", "16:00", "alemanha", estadio(4), "cgrupoe")
   )}
   ${createCard(
     "28/11",
     "segunda",
-    createGame("camarões", "07:00", "sérvia", estadio(3) /*group g*/) +
-      createGame("coreia do sul", "10:00", "gana", estadio(7) /*group h*/) +
-      createGame("brasil", "13:00", "suíça", estadio(6) /*group g*/) +
-      createGame("portugal", "16:00", "uruguai", estadio(0) /*group h*/)
+    createGame("camarões", "07:00", "sérvia", estadio(3), "cgrupog") +
+      createGame("coreia do sul", "10:00", "gana", estadio(7), "cgrupoh") +
+      createGame("brasil", "13:00", "suíça", estadio(6), "cgrupog") +
+      createGame("portugal", "16:00", "uruguai", estadio(0), "cgrupoh")
   )}
   ${createCard(
     "29/11",
     "terça",
-    createGame("equador", "07:00", "senegal", estadio(1) /*group a*/) +
-      createGame("holanda", "10:00", "catar", estadio(4) /*group a*/) +
-      createGame("irã", "13:00", "estados unidos", estadio(2) /*group b*/) +
-      createGame("gales", "16:00", "inglaterra", estadio(5) /*group b*/)
+    createGame("equador", "07:00", "senegal", estadio(1), "cgrupoa") +
+      createGame("holanda", "10:00", "catar", estadio(4), "cgrupoa") +
+      createGame("irã", "13:00", "estados unidos", estadio(2), "cgrupob") +
+      createGame("gales", "16:00", "inglaterra", estadio(5), "cgrupob")
   )}
   ${createCard(
     "30/11",
     "quarta",
-    createGame("tunísia", "07:00", "frança", estadio(7) /*group d*/) +
-      createGame("austrália", "10:00", "dinamarca", estadio(3) /*group d*/) +
-      createGame("polônia", "13:00", "argentina", estadio(6) /*group c*/) +
-      createGame("arábia saudita", "16:00", "méxico", estadio(0) /*group c*/)
+    createGame("tunísia", "07:00", "frança", estadio(7), "cgrupod") +
+      createGame("austrália", "10:00", "dinamarca", estadio(3), "cgrupod") +
+      createGame("polônia", "13:00", "argentina", estadio(6), "cgrupoc") +
+      createGame("arábia saudita", "16:00", "méxico", estadio(0), "cgrupoc")
   )}
   ${createCard(
     "01/11",
     "quinta",
-    createGame("croácia", "07:00", "bélgica", estadio(5) /*group f*/) +
-      createGame("canadá", "10:00", "marrocos", estadio(2) /*group f*/) +
-      createGame("japão", "13:00", "espanha", estadio(1) /*group e*/) +
-      createGame("costa rica", "16:00", "alemanha", estadio(4) /*group e*/)
+    createGame("croácia", "07:00", "bélgica", estadio(5), "cgrupof") +
+      createGame("canadá", "10:00", "marrocos", estadio(2), "cgrupof") +
+      createGame("japão", "13:00", "espanha", estadio(1), "cgrupoe") +
+      createGame("costa rica", "16:00", "alemanha", estadio(4), "cgrupoe")
   )}
   ${createCard(
     "02/12",
     "sexta",
-    createGame("coreia do sul", "07:00", "portugal", estadio(7) /*group h*/) +
-      createGame("gana", "10:00", "uruguai", estadio(3) /*group h*/) +
-      createGame("sérvia", "13:00", "suíça", estadio(6) /*group g*/) +
-      createGame("camarões", "16:00", "brasil", estadio(0) /*group g*/)
+    createGame("coreia do sul", "07:00", "portugal", estadio(7), "cgrupoh") +
+      createGame("gana", "10:00", "uruguai", estadio(3), "cgrupoh") +
+      createGame("sérvia", "13:00", "suíça", estadio(6), "cgrupog") +
+      createGame("camarões", "16:00", "brasil", estadio(0), "cgrupog")
   )}
 </main>
 `
